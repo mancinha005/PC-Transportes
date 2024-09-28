1e9bf2606c43d84b2a1630443037b7cf49e42384
Luan Martins <57342190+mancinha005@users.noreply.github.com>
2024-09-28 00:50:09 -0300

    subi os arquivos do site

---
+++ pc-transportes/js/menu.js
@@ -0,0 +1,13 @@
+
+let navBar = document.querySelector('#header')
+
+document.addEventListener("scroll", ()=>{
+    let scrolltop = window.scrollY
+
+    if(scrolltop > 0){
+        navBar.classList.add('rolar')
+    }else{
+        navBar.classList.remove('rolar')
+    }
+
+})
